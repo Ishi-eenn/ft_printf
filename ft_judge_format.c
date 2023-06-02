389345fbb291f3bed06997ae6ca9adb757db39f0
Ishi <syi378039@gmail.com>
2023-06-03 01:46:22 +0900

    add : ft_judge_format.c

--- ft_judge_format.c
+++ ft_judge_format.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 11:54:03 by tsishika          #+#    #+#             */
-/*   Updated: 2023/06/03 01:41:34 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/06/03 01:45:32 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,26 @@
 
 int	ft_judge_format(va_list *data, const char format)
 {
-	int print_len;
+	int	print_len;
 
 	print_len = 0;
-	if(format == 'c')
+	if (format == 'c')
 		print_len += ft_putchar(va_arg(*data, int));
-	if(format == 's')
+	if (format == 's')
 		print_len += ft_putstr(va_arg(*data, char *));
-	if(format == 'p')
+	if (format == 'p')
 		print_len += ft_putptr(va_arg(*data, uintptr_t));
-	if(format == 'd')
+	if (format == 'd')
 		print_len += ft_putnbr(va_arg(*data, int));
-	if(format == 'i')
+	if (format == 'i')
 		print_len += ft_putnbr(va_arg(*data, int));
-	if(format == 'u')
+	if (format == 'u')
 		print_len += ft_putunbr(va_arg(*data, unsigned int));
-	if(format == 'x')
+	if (format == 'x')
 		print_len += ft_lowercase_puthex(va_arg(*data, unsigned int));
-	if(format == 'X')
+	if (format == 'X')
 		print_len += ft_uppercase_puthex(va_arg(*data, unsigned int));
-	if(format == '%')
+	if (format == '%')
 		print_len += write(1, "%", 1);
 	return (print_len);
 }
