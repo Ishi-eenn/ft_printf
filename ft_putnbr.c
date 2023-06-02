7c82928b4f9664f3a1cb88d153e42a59e05d4b26
Ishi <syi378039@gmail.com>
2023-06-03 01:25:42 +0900

    add : ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 16:47:37 by tsishika          #+#    #+#             */
-/*   Updated: 2023/05/30 12:40:31 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/06/03 01:25:05 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,16 @@ static int	ft_intlen(int n)
 
 int	ft_putnbr(int nb)
 {
-	long n = nb;
-	if(n < 0){
+	long	n;
+
+	n = nb;
+	if (n < 0)
+	{
 		n *= -1;
 		ft_putchar('-');
 	}
-	if(n >= 10){
+	if (n >= 10)
+	{
 		ft_putnbr(n / 10);
 		ft_putnbr(n % 10);
 	}
@@ -50,8 +54,11 @@ int	ft_putnbr(int nb)
 
 int	ft_putunbr(unsigned int nb)
 {
-	unsigned int n = nb;
-	if(n >= 10){
+	unsigned int	n;
+
+	n = nb;
+	if (n >= 10)
+	{
 		ft_putunbr(n / 10);
 		ft_putunbr(n % 10);
 	}
@@ -59,14 +66,3 @@ int	ft_putunbr(unsigned int nb)
 		ft_putchar(n + '0');
 	return (ft_intlen(nb));
 }
-
-
-
-
-// #include <stdio.h>
-
-// int main(void){
-// 	// int len = ft_putint(13);
-// 	printf("  %d  \n", ft_putnbr(-2147483648));
-// 	return (0);
-// }
