3fd9b5363ab41d9e9ee7b2f5ba4c76b0697094b1
Ishi <syi378039@gmail.com>
2023-06-03 03:38:56 +0900

    add : ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,20 +6,20 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 16:47:37 by tsishika          #+#    #+#             */
-/*   Updated: 2023/06/03 01:25:05 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/06/03 03:00:03 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_intlen(int n)
+static int	ft_intlen(int nb)
 {
 	int		len;
 	long	buff_n;
 
-	buff_n = n;
+	buff_n = nb;
 	len = 0;
-	if (0 >= n)
+	if (0 >= nb)
 	{
 		buff_n *= -1;
 		len++;
@@ -46,21 +46,6 @@ int	ft_putnbr(int nb)
 	{
 		ft_putnbr(n / 10);
 		ft_putnbr(n % 10);
-	}
-	else
-		ft_putchar(n + '0');
-	return (ft_intlen(nb));
-}
-
-int	ft_putunbr(unsigned int nb)
-{
-	unsigned int	n;
-
-	n = nb;
-	if (n >= 10)
-	{
-		ft_putunbr(n / 10);
-		ft_putunbr(n % 10);
 	}
 	else
 		ft_putchar(n + '0');
