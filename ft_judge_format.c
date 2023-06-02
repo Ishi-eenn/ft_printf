e55c9e8688ddec45a2be3d493e4162799ffdfea4
Ishi <syi378039@gmail.com>
2023-06-03 01:51:57 +0900

    add : ft_judge_format.c

--- ft_judge_format.c
+++ ft_judge_format.c
@@ -6,13 +6,11 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 11:54:03 by tsishika          #+#    #+#             */
-/*   Updated: 2023/06/03 01:45:32 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/06/03 01:51:24 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 int	ft_judge_format(va_list *data, const char format)
 {
